d7843d1e4df79b2c948066ee004e554379d1b2da
capcesar23 <cesar_a_pereira@hotmail.com>
2022-11-25 05:10:13 -0300

    adicionando o calendario, e mostrando na tela dia mes e ano

--- Calendario/app/src/main/java/com/example/calendario/MainActivity.java
+++ Calendario/app/src/main/java/com/example/calendario/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.calendario;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.calendario.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.submitButton.setOnClickListener(v -> {
+            String dia = "Dia = " + binding.date.getDayOfMonth();
+            String mes = "Mês = " + binding.date.getMonth();
+            String ano = "Ano = " + binding.date.getYear();
+
+            Toast.makeText(getApplicationContext(), dia + "/" + mes + "/" + ano , Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
